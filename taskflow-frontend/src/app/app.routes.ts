368112b87b74c5fa555d2ba49bb08b0e2a52ad66
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-07 13:38:31 +0530

    Added Profile Routes

--- taskflow-frontend/src/app/app.routes.ts
+++ taskflow-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -11,6 +12,11 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
     canActivate: [authGuard]
   },
   { path: '**', redirectTo: '/login' }
