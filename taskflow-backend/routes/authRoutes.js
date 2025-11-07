b9e4ecb8cec02c572ff066404b2123c80363e2f9
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-07 13:32:25 +0530

    Added Profile Routes

--- taskflow-backend/routes/authRoutes.js
+++ taskflow-backend/routes/authRoutes.js
@@ -4,6 +4,8 @@ const {
   register,
   login,
   getCurrentUser,
+  updateProfile,
+  changePassword,
 } = require("../controllers/authController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
@@ -39,6 +41,38 @@ router.post(
     body("password").notEmpty().withMessage("Password is required"),
   ],
   login
+);
+
+// PUT /api/auth/profile - Update user profile (protected route)
+router.put(
+  "/profile",
+  authMiddleware,
+  [
+    body("name")
+      .trim()
+      .isLength({ min: 2 })
+      .withMessage("Name must be at least 2 characters long"),
+    body("email")
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("Please provide a valid email address"),
+  ],
+  updateProfile
+);
+
+// PUT /api/auth/password - Change password (protected route)
+router.put(
+  "/password",
+  authMiddleware,
+  [
+    body("currentPassword")
+      .notEmpty()
+      .withMessage("Current password is required"),
+    body("newPassword")
+      .isLength({ min: 6 })
+      .withMessage("New password must be at least 6 characters long"),
+  ],
+  changePassword
 );
 
 // Get current user (protected route)
